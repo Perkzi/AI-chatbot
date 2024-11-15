6026b2aa03a8dad8cb09ec5a29b033712a377d0b
Perkzi <1520518697@qq.com>
2024-11-15 15:14:15 +0800

    initialize

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import VueClipBoard from 'vue-clipboard2'
+
+Vue.use(VueClipBoard)
+//use：将第三方模块 注入到Vue实例对象中的方法
+Vue.use(VueAxios, axios)
+
+// 设置 Axios 的默认基础 URL
+axios.defaults.baseURL = 'http://localhost:3000'; // 替换后端地址和端口
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
